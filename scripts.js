e1595b91216de663b1809ff76a82c675634f8037
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-11-22 13:22:34 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function angryFish() {
+    document.getElementById('img1')
+    .src="https://thesoftroots.com/wp-content/uploads/2022/04/Angry-fish-coloring-pages.jpg"
+    document.getElementById('message')
+    .innerHTML="Angry Fish"
+}
+
+function happyFish() {
+    document.getElementById('img1')
+    .src="https://cdn2.vectorstock.com/i/1000x1000/52/96/happy-fish-cartoon-icon-vector-13755296.jpg"
+    document.getElementById('message')
+    .innerHTML="Happy Fish"
+}
+
+function oceanFun() {
+    document.getElementById('img1')
+    .src="https://en.pimg.jp/066/542/320/1/66542320.jpg"
+    document.getElementById('message')
+    .innerHTML="Ocean Fun"
+}
